03b3c94a3adfa9dd9b6242ed39e7ac17879f92f5
Dolunay <admin@gelistirici.com>
2026-02-26 01:27:40 +0300

    Set ChmViewer root namespace

--- app/ChmViewer/ProgramPublishSingleFile.cs
+++ app/ChmViewer/ProgramPublishSingleFile.cs
@@ -1,10 +1,9 @@
 using CefSharp;
 using CefSharp.Wpf;
-using ChmViewer;
 using System;
 using System.IO;
 
-namespace HelpSample;
+namespace ChmViewer;
 // Copyright © 2021 The CefSharp Authors. All rights reserved.
 //
 // Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
